d95923a712eecd15843b0faf6eccd5947c1d955d
rumeysa <rumey@DESKTOP-M5AHE8G>
2021-01-23 23:12:09 +0300

    16 ocak ders sonu

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Rümeysa";
+            int yas = 21;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "Java";
+            kurs1.Eğitmen = "Rümeysa Agacbacak";
+            kurs1.IzlenmeOrani = 92;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "C#";
+            kurs2.Eğitmen = "Engin Demiroğ";
+            kurs2.IzlenmeOrani = 68;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Pythıon";
+            kurs3.Eğitmen = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 86;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Eğitmen = "Murat Kurtboğan";
+            kurs4.IzlenmeOrani = 100;
+
+            // Console.WriteLine(kurs1.KursAdi + " : " + kurs.Eğitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi+ " : " + kurs.Eğitmen);
+
+            }
+            
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Eğitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Dongüler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Dongüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirme Yetiştirme Kampı";
+            string kurs2 = "Programlama başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array-dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirme Yetiştirme Kampı", "Programlama başlangıç için temel kurs","Java","python",
+            "C++"};
+                
+
+            for (int i = 0; i <kurslar.Length; i++) //length eleman sayısı 
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti.");
+
+            foreach (string kurs in kurslar) //dizilere uygulanır .diziler tek tek sorguluyor.
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
