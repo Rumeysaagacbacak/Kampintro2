e9f9f127f5434902ab1afbcd0d493ccfcfd373a9
rumeysa <rumey@DESKTOP-M5AHE8G>
2021-01-25 00:19:32 +0300

    20 ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor ----bir class newlendiğinde çalışan bloğa denirr
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;// geçici array
+            items= new T[items.Length+1];//eleman sayısını verir.
+            for (int i = 0; i < tempArray.Length; i++)//geçici arraydan geri çagırmak için yazdığımız kod.
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //snippet -hazır kodlar .
+        public int Id { get; set; }
+        public int CategoryId { get; set; } //kategori ıd
+        public string ProductName { get; set; }//üürn ismi
+        public double UnitPrice { get; set; } //ürünün birim fiyatı
+        public int UnitInStok  { get; set; } //ürün stok adedi
+
+        
+    }
+}
